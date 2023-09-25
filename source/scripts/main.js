4834402b11c8c1865b12bc0fe7bc0d0ec7e8b4d1
Dev Wandy <gabriel_wanderson@hotmail.com>
2023-09-25 10:00:52 -0300

    Fix: ajustando lógica IMC.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,5 +1,10 @@
 const form = document.querySelector('form');
-
+let linhas = '';
+const imagemTriste = '<img src="source/images/smile-sad.png" alt="Emoji triste" />';
+const imagemFeliz = '<img src="source/images/smile.png" alt="Emoji feliz"/>';
+const imagemEntediado = '<img src="source/images/smile-bored.png" alt="Emoji entediado" />';
+const descricaoImc = document.querySelector('.description-imc');
+const imagemResultado = document.querySelector('.img-resultado');
 
 function calculeImc(peso, altura) {
     peso = parseFloat (peso);
@@ -18,20 +23,28 @@ form.addEventListener('submit', function(e){
 
     const resultadoImc = calculeImc(valorPeso.value, valorAltura.value);
     const resultadoImcConvertidoEmFloat = parseFloat(resultadoImc);
+    const mensagemResultadoImcPositivo = `<p> Seu IMC é de: <span class="spanSuccess">${resultadoImcConvertidoEmFloat}</span></p>`
+    const mensagemResultadoImcAtencao = `<p> Seu IMC é de: <span class="spanAttention">${resultadoImcConvertidoEmFloat}</span></p>`
+    const mensagemResultadoImcNegativo = `<p> Seu IMC é de: <span class="spanWarning">${resultadoImcConvertidoEmFloat}</span></p>`
 
-    console.log ("Resultado IMC: "+resultadoImcConvertidoEmFloat);
     
         if (resultadoImcConvertidoEmFloat < 18.5){
-            console.log ("Abaixo do Peso!");
-        } else if (resultadoImcConvertidoEmFloat >= 18.6 && resultadoImcConvertidoEmFloat <= 24.9){
-            console.log ("Parabéns, peso ideal!");
+            descricaoImc.innerHTML = mensagemResultadoImcAtencao;
+            imagemResultado.innerHTML = imagemEntediado;
+        } else if (resultadoImcConvertidoEmFloat >= 18.5 && resultadoImcConvertidoEmFloat <= 24.9){
+            descricaoImc.innerHTML = mensagemResultadoImcPositivo;
+            imagemResultado.innerHTML = imagemFeliz;
         } else if (resultadoImcConvertidoEmFloat >= 25.0 && resultadoImcConvertidoEmFloat <= 29.9){
-            console.log ("Levemente acima do Peso!");
+            descricaoImc.innerHTML = mensagemResultadoImcNegativo;
+            imagemResultado.innerHTML = imagemTriste;
         } else if (resultadoImcConvertidoEmFloat >= 30.0 && resultadoImcConvertidoEmFloat <= 34.9) {
-            console.log ("Obesidade Grau I");
+            descricaoImc.innerHTML = mensagemResultadoImcNegativo;
+            imagemResultado.innerHTML = imagemTriste;
         } else if (resultadoImcConvertidoEmFloat >= 35.0 && resultadoImcConvertidoEmFloat <= 39.9) {
-            console.log ("Obesidade Grau II (Severa)");
+            descricaoImc.innerHTML = mensagemResultadoImcNegativo;
+            imagemResultado.innerHTML = imagemTriste;
         } else {
-            console.log ("Osedidade Grau III (Mórbida)");
+            descricaoImc.innerHTML = mensagemResultadoImcNegativo;
+            imagemResultado.innerHTML = imagemTriste;
         }
 })
